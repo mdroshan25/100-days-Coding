9a5f8157363d74ff1719e28903e91c12845b17d3
Roshan Moo <50206992+mdroshan25@users.noreply.github.com>
2021-09-03 15:01:51 +0800

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,27 @@
+#print("for loop")
+#word = ["this", "is","roshan"]
+
+# for w in word:   print(w, len(w))
+    
+#give range of each word for w in range(len(word)): print(w, word[w])
+
+#get odd or even
+"""
+for x in range(0,5):
+    x+=1
+    #print(x)
+    if x % 2 == 0:
+        print(f"{x}::Even No")
+    else:
+        print(f"{x}::Odd No")
+
+print("PRime No's")
+for n in range(1,10):
+    for x in range(2,n):
+        if n % x ==0:
+            
+            print(f"{n}  get by multiple of {x}  and is divisible of {x}, So it is not a prime no")
+            break
+    else:
+        print(f"{n} is prime no")
+"""
